1bac5254f559ca91c7ad0123265db47e3ca319fe
francesco <francescosanti91@gmail.com>
2021-11-02 15:37:49 +0100

    creazione campo da gioco

--- js/script.js
+++ js/script.js
@@ -18,3 +18,46 @@
         // agganciare a ogni cella l'evento "click"
             // cambio lo sfondo della cella cliccata
             // mostro il numero della cella cliccata
+
+
+// variabili globali
+let difficoltaUtente;
+
+// Chiedo all'utente con quale livello di difficoltà vuole giocare (facile, medio, difficile)
+// Salvo la risposta dell'utente
+difficoltaUtente = prompt("Scegli un livello di difficoltà (facile, medio, difficile)");
+
+// A seconda del livello scelto, creo un numero di celle corrispondente
+// Confronto il livello scelto dall'utente con i tre livelli di difficoltà
+if (difficoltaUtente === "facile") {
+
+    generaCampoMinato(100, "facile");
+
+} else if (difficoltaUtente === "medio") {
+
+    generaCampoMinato(81, "medio");
+
+} else if (difficoltaUtente === "difficile") {
+
+    generaCampoMinato(49, "difficile");
+
+}
+
+// Siccome devo ripetere le stesse istruzioni, creo una funzione per generare il campo minato
+function generaCampoMinato(numCelle, livelloGioco) {
+
+    // creo un ciclo per:
+    for (let i = 1; i <= numCelle; i++) {
+
+        console.log("numero cella:", i);
+
+        // generare il numero di celle corrispondenti al livello di difficoltà
+        let nuovaCella = document.createElement("div");
+        nuovaCella.className = "cella " + livelloGioco;
+        document.getElementById("contenitore-celle").append(nuovaCella);
+        
+        // agganciare a ogni cella l'evento "click"
+        // cambio lo sfondo della cella cliccata
+        // mostro il numero della cella cliccata
+    }
+}
